a2867df3faccb06d5734236a1dafe28700704164
celebigilfatih <fatihcelebigil@gmail.com>
2022-05-28 16:17:02 +0300

    career sayfasi yapildi

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,85 @@
-const gulp                      = require('gulp'),
-      del                       = require('del'),
-      sourcemaps                = require('gulp-sourcemaps'),
-      plumber                   = require('gulp-plumber'),
-      sass                      = require('gulp-sass'),
-      autoprefixer              = require('gulp-autoprefixer'),
-      minifyCss                 = require('gulp-clean-css'),
-      fileinclude               = require('gulp-file-include');
-      imagemin                  = require('gulp-imagemin'),
-      browserSync               = require('browser-sync').create(),
-      dependents                = require('gulp-dependents'),
-      src_folder                = './src/',
-      src_assets_folder         = src_folder + 'assets/',
-      dist_folder               = './dist/',
-      dist_assets_folder        = dist_folder + 'assets/',
-      node_modules_folder       = './node_modules/',
-      dist_node_modules_folder  = dist_folder + 'node_modules/',
+const gulp = require("gulp"),
+  del = require("del"),
+  sourcemaps = require("gulp-sourcemaps"),
+  plumber = require("gulp-plumber"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  minifyCss = require("gulp-clean-css"),
+  fileinclude = require("gulp-file-include");
+(imagemin = require("gulp-imagemin")),
+  (browserSync = require("browser-sync").create()),
+  (dependents = require("gulp-dependents")),
+  (src_folder = "./src/"),
+  (src_assets_folder = src_folder + "assets/"),
+  (dist_folder = "./dist/"),
+  (dist_assets_folder = dist_folder + "assets/"),
+  (node_modules_folder = "./node_modules/"),
+  (dist_node_modules_folder = dist_folder + "node_modules/"),
+  (node_dependencies = Object.keys(
+    require("./package.json").dependencies || {}
+  ));
 
-      node_dependencies         = Object.keys(require('./package.json').dependencies || {});
+gulp.task("clear", () => del([dist_folder]));
 
-gulp.task('clear', () => del([ dist_folder ]));
-
-gulp.task('html', () => {
-  return gulp.src([ src_folder + '/src/**/*.html' ], {
-    base: src_folder,
-    since: gulp.lastRun('html')
-  })
+gulp.task("html", () => {
+  return gulp
+    .src([src_folder + "/**/*.html"], {
+      base: src_folder,
+      since: gulp.lastRun("html"),
+    })
     .pipe(gulp.dest(dist_folder))
     .pipe(browserSync.stream());
 });
 
-gulp.task('fileinclude', () => {
-  return gulp.src([src_folder + '**/*.html'], { since: gulp.lastRun('fileinclude')})
-  .pipe(fileinclude({
-    prefix: '@@',
-    basepath: '@file'
-  }))
-  .pipe(gulp.dest(dist_folder))
-  .pipe(browserSync.stream());
-})
+gulp.task("fileinclude", () => {
+  return gulp
+    .src([src_folder + "**/*.html"], { since: gulp.lastRun("fileinclude") })
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest(dist_folder))
+    .pipe(browserSync.stream());
+});
 
-
-gulp.task('sass', () => {
-  return gulp.src([
-    src_assets_folder + 'scss/**/*.scss'
-  ], { since: gulp.lastRun('sass') })
+gulp.task("sass", () => {
+  return gulp
+    .src([src_assets_folder + "scss/**/*.scss"], {
+      since: gulp.lastRun("sass"),
+    })
     .pipe(sourcemaps.init())
-      .pipe(plumber())
-      .pipe(dependents())
-      .pipe(sass())
-      .pipe(autoprefixer())
-      .pipe(minifyCss())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dist_assets_folder + 'css'))
-    .pipe(browserSync.stream());
-});
-
-
-
-gulp.task('js', () => {
-  return gulp.src([ src_assets_folder + 'js/**/*.js' ], { since: gulp.lastRun('js') })
     .pipe(plumber())
-    .pipe(gulp.dest(dist_assets_folder + 'js'))
+    .pipe(dependents())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(dist_assets_folder + "css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('images', () => {
-  return gulp.src([ src_assets_folder + 'images/**/*.+(png|jpg|jpeg|gif|svg|ico)' ], { since: gulp.lastRun('images') })
+gulp.task("js", () => {
+  return gulp
+    .src([src_assets_folder + "js/**/*.js"], { since: gulp.lastRun("js") })
+    .pipe(plumber())
+    .pipe(gulp.dest(dist_assets_folder + "js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("images", () => {
+  return gulp
+    .src([src_assets_folder + "images/**/*.+(png|jpg|jpeg|gif|svg|ico)"], {
+      since: gulp.lastRun("images"),
+    })
     .pipe(plumber())
     .pipe(imagemin())
-    .pipe(gulp.dest(dist_assets_folder + 'images'))
+    .pipe(gulp.dest(dist_assets_folder + "images"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('vendor', () => {
+gulp.task("vendor", () => {
   if (node_dependencies.length === 0) {
     return new Promise((resolve) => {
       console.log("No dependencies specified");
@@ -80,48 +87,61 @@ gulp.task('vendor', () => {
     });
   }
 
-  return gulp.src(node_dependencies.map(dependency => node_modules_folder + dependency + '/**/*.*'), {
-    base: node_modules_folder,
-    since: gulp.lastRun('vendor')
-  })
+  return gulp
+    .src(
+      node_dependencies.map(
+        (dependency) => node_modules_folder + dependency + "/**/*.*"
+      ),
+      {
+        base: node_modules_folder,
+        since: gulp.lastRun("vendor"),
+      }
+    )
     .pipe(gulp.dest(dist_node_modules_folder))
     .pipe(browserSync.stream());
 });
 
-gulp.task('build', gulp.series('clear', 'html', 'fileinclude', 'sass', 'js', 'images', 'vendor'));
+gulp.task(
+  "build",
+  gulp.series("clear", "html", "fileinclude", "sass", "js", "images", "vendor")
+);
 
-gulp.task('dev', gulp.series('html', 'fileinclude', 'sass', 'js'));
+gulp.task("dev", gulp.series("html", "fileinclude", "sass", "js"));
 
-gulp.task('serve', () => {
+gulp.task("serve", () => {
   return browserSync.init({
     server: {
-      baseDir: [ 'dist' ]
+      baseDir: ["dist"],
     },
     port: 3000,
-    open: false
+    open: false,
   });
 });
 
-gulp.task('watch', () => {
+gulp.task("watch", () => {
   const watchImages = [
-    src_assets_folder + 'images/**/*.+(png|jpg|jpeg|gif|svg|ico)'
+    src_assets_folder + "images/**/*.+(png|jpg|jpeg|gif|svg|ico)",
   ];
 
   const watchVendor = [];
 
-  node_dependencies.forEach(dependency => {
-    watchVendor.push(node_modules_folder + dependency + '/**/*.*');
+  node_dependencies.forEach((dependency) => {
+    watchVendor.push(node_modules_folder + dependency + "/**/*.*");
   });
 
   const watch = [
-    src_folder + '**/*.html',
-    src_assets_folder + 'scss/**/*.scss',
-    src_assets_folder + 'js/**/*.js'
+    src_folder + "**/*.html",
+    src_assets_folder + "scss/**/*.scss",
+    src_assets_folder + "js/**/*.js",
   ];
 
-  gulp.watch(watch, gulp.series('dev')).on('change', browserSync.reload);
-  gulp.watch(watchImages, gulp.series('images')).on('change', browserSync.reload);
-  gulp.watch(watchVendor, gulp.series('vendor')).on('change', browserSync.reload);
+  gulp.watch(watch, gulp.series("dev")).on("change", browserSync.reload);
+  gulp
+    .watch(watchImages, gulp.series("images"))
+    .on("change", browserSync.reload);
+  gulp
+    .watch(watchVendor, gulp.series("vendor"))
+    .on("change", browserSync.reload);
 });
 
-gulp.task('default', gulp.series('build', gulp.parallel('serve', 'watch')));
+gulp.task("default", gulp.series("build", gulp.parallel("serve", "watch")));
